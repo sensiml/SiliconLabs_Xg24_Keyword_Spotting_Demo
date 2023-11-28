8aa0532da80ddcef63214facf6edc77d99bee054
Chris Knorowski <cknorow@gmail.com>
2023-11-28 10:07:54 -0800

    Update sml_output.h

--- knowledgepack/application/sml_output.h
+++ knowledgepack/application/sml_output.h
@@ -6,8 +6,8 @@
 #include <string.h>
 
 
-uint32_t sml_output_results(uint16_t model, uint16_t classification);
+void sml_output_results(uint16_t model, uint16_t classification);
 
-uint32_t sml_output_init(void * p_module);
+
 
 #endif //__SML_OUTPUT_H_
