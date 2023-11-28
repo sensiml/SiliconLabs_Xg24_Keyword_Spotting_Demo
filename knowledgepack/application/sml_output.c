6726bc66fba0d97075796fc582e3a6965ab8eb4b
Chris Knorowski <cknorow@gmail.com>
2023-11-28 10:07:39 -0800

    Update sml_output.c

--- knowledgepack/application/sml_output.c
+++ knowledgepack/application/sml_output.c
@@ -15,11 +15,12 @@ float recent_fv_times[MAX_VECTOR_SIZE];
 uint32_t recent_fv_cycles[MAX_VECTOR_SIZE];
 #endif
 
-static void sml_output_serial(uint16_t model)
+void sml_output_results(uint16_t model, uint16_t classification)
 {
+    int32_t written = 0;
     memset(serial_out_buf, 0, SERIAL_OUT_CHARS_MAX);
 
-    kb_sprint_model_result(model, serial_out_buf, false, false, true);
+    kb_sprint_model_result(model, serial_out_buf, true, true, true);
 
     printf("%s\n", serial_out_buf);
 #if SML_PROFILER
@@ -30,18 +31,4 @@ static void sml_output_serial(uint16_t model)
     kb_print_model_times(model, serial_out_buf, recent_fv_times);
     printf("%s\n", serial_out_buf);
 #endif
-}
-
-uint32_t sml_output_results(uint16_t model, uint16_t classification)
-{
-    memset(serial_out_buf, 0, SERIAL_OUT_CHARS_MAX);
-    sml_output_serial(model);
-
-    return 0;
-}
-
-uint32_t sml_output_init(void *p_module)
-{
-    // unused for now
-    return 0;
 }
